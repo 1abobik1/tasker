fee2ea35f21c821c3b433f17eaa0666547ba25ac
1abobik1 <dima15042004@mail.ru>
2025-04-20 10:06:53 +0300

    add conf

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	
+}
+
+func MustLoad() *Config {
+	path := getConfigPath()
+
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exists" + path)
+	}
+
+	err := godotenv.Load(path)
+	if err != nil {
+		panic(fmt.Sprintf("No .env file found at %s, relying on environment variables", path))
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		panic(fmt.Sprintf("Failed to load environment variables: %v", err))
+	}
+
+	return &cfg
+}
+
+func getConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	return res
+}
